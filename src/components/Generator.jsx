c567b4ddcca991f21a1b4d6bc1039fd1fafedd63
Awashek Acharya <awashek.acharya16@gmail.com>
2024-08-23 11:35:19 +0545

    commit

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -16,22 +16,38 @@ function Header(props) {
 }
 
 export default function Generator() {
+  let showModel = false
   return (
     <SectionWrapper header={"generate your workout"} title=
       {['It\'s', 'Huge','o\'clock']}>
-      <Header index={'01'} title={'Pick your poison'} 
-      description={'Select the workout you wish to enroll'}/>
-      <div className='grid grid-cols-2 sm:grid-cols-4
-      gap-4'>
-        {Object.keys(WORKOUTS).map((type, typeIndex) => {
-        return (
-          <button className='bg-slate-950 border
-          border-blue-400 py-3 rounded-lg' key={typeIndex}>
-            <p>{type}</p>
-          </button>
-        )
-      })}
-      </div>
+        <Header index={'01'} title={'Pick your poison'} 
+        description={'Select the workout you wish to enroll'}/>
+        <div className='grid grid-cols-2 sm:grid-cols-4
+        gap-4'>
+          {Object.keys(WORKOUTS).map((type, typeIndex) => {
+          return (
+            <button className='bg-slate-950 border
+            border-blue-400 py-3 rounded-lg
+            duration-200 hover:border-blue-600' key={typeIndex}>
+              <p className='capatalize'>{type.replaceAll('_',' ')}</p>
+            </button>
+          )
+        })}
+        </div>
+        <Header index={'02'} title={'Lock on targets'} 
+        description={'Select the muscles judge for annihilation'}/>
+        <div className='bg-slate-950 p-3 border border-solid
+        border-blue-400 rounded-lg'>
+          <div className='relative flex items-center
+          justify-center'> 
+            <p>Select muscle groups</p>
+            <i className="fa-solid absolute right-3
+            top-1/2 -translate-y-1/2 fa-caret-down "></i>
+          </div>
+          {showModel && (
+            <div> model</div>
+          )}
+        </div>
     </SectionWrapper>
   )
 }
