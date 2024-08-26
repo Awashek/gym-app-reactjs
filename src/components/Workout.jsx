df7105f6e6b41dd54b0812556d4488a0be852282
Awashek Acharya <awashek.acharya16@gmail.com>
2024-08-26 22:33:06 +0545

    commit

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
-
+import SectionWrapper from './SectionWrapper'
 export default function Workout(props) {
+  
   const {workout} = props
   return (
-    <div>Workout</div>
+       <SectionWrapper header={"Welcome to"} title=
+      {['The', 'DANGER','zone']}></SectionWrapper>
   )
 }
